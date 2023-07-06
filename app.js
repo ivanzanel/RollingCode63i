4fdb751f4f767ef47c868d9824a049243b54f2c9
ivanzanel <ivan.zanel@hotmail.com>
2023-07-06 15:52:33 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,25 @@
+"use srict"
+
+console.log("Hola mundo");
+
+
+let usuario = 'Pepe';
+usuario = 'Juan';
+usuario = 5;
+
+console.log(usuario);
+
+
+//alert ("el nombre que ingresaste es: ") + respuestaUsuario;
+
+
+console.log(respuestaUsuario)
+
+let respuestaUsuario;
+respuestaUsuario = prompt("¿Cuál es tu nombre?")
+
+let=hola;
+
+
+
+document.write ('hola')
