81cb5753b8272b517808ff1b966128057e607600
ivanzanel <62265767+ivanzanel@users.noreply.github.com>
2023-07-27 12:37:52 -0300

    Correcion ejercicio 3

--- ejerciciosTP2/ex3/app.js
+++ ejerciciosTP2/ex3/app.js
@@ -1,13 +1,16 @@
 // 3- Realiza un script que pida cadenas de texto  hasta que se pulse “cancelar”. Al salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un guión -.
 
-"use strict"
 
-let acumulador= '';
-while (true){
-    let texto= prompt("Coloque un texto:");
+"use strict";
+
+let acumulador = '';
+while (true) {
+    let texto = prompt("Coloque un texto:");
+
     if (texto === null) {
         break;
     }
-    acumulador= acumulador + " - " + texto
+
+    acumulador += texto + " - ";
     console.log(acumulador);
 }
