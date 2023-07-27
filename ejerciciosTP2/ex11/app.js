7ddb43aa5e6608be0828fe7885d094196c8ea8ed
ivanzanel <62265767+ivanzanel@users.noreply.github.com>
2023-07-27 11:39:03 -0300

    Correccion Ejercicio 11

--- ejerciciosTP2/ex11/app.js
+++ ejerciciosTP2/ex11/app.js
@@ -1,23 +1,26 @@
 // 11- Realiza un script que pida por teclado 3 edades y 3 nombres e indique el nombre del mayor.
 
 
-"use strict"
+"use strict";
 
-let edades = [];
+let edad1 = prompt("Ingrese una edad"); 
+let edad2 = prompt("Ingrese una edad");
+let edad3 = prompt("Ingrese otra edad"); 
 
-edades.push(edad1);
-edades.push(edad2);
-edades.push(edad3);
-
+let edades = [edad1, edad2, edad3];
 
 let mayor = Math.max(edad1, edad2, edad3);
+
+let nombre1 = "Eze"; 
+let nombre2 = "Gabi"; 
+let nombre3 = "Ivan"; 
 
 if (edad1 === mayor) {
     console.log(`La persona mayor es: ${nombre1}`);
 } else if (edad2 === mayor) {
     console.log(`La persona mayor es: ${nombre2}`);
-} else{
+} else {
     console.log(`La persona mayor es: ${nombre3}`);
-
 }
-console.log(mayor);
+
+console.log(`La edad mayor es: ${mayor}`);
