6cfc7ea64a660539118bb875517c17b368cb93ba
ivanzanel <62265767+ivanzanel@users.noreply.github.com>
2023-07-27 12:25:28 -0300

    Correcion Ejercicio 4 -m

--- ejerciciosTP2/ex4/app.js
+++ ejerciciosTP2/ex4/app.js
@@ -1,17 +1,22 @@
-"use strict"
+// 4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
 
-let acumulador= 0;
-while (true){
-    let numero=prompt("Coloque un número:");
-    if (numero===null) {
+"use strict";
+
+let acumulador = 0;
+while (true) {
+    let numero = prompt("Coloque un número:");
+
+    if (numero === null) {
         break;
     }
-    numero=Number(numero)
-    if(isNan(numero)){
-        console.log("Ingrese un caracter válido")
-        continue
+
+    numero = Number(numero);
+
+    if (isNaN(numero)) {
+        window.alert("Ingrese un número válido");
+        continue;
     }
 
-    acumulador= numero
-    console.log(acumulador)
+    acumulador += numero;
+    console.log(acumulador);
 }
