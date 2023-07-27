c4af5ec8efec95fc43934c9cfcd682c7bd7407a4
ivanzanel <62265767+ivanzanel@users.noreply.github.com>
2023-07-27 11:28:43 -0300

    Correcion ejercicio 14

--- ejerciciosTP2/ex14/app.js
+++ ejerciciosTP2/ex14/app.js
@@ -1,12 +1,16 @@
 // 14- Realiza un script que pida una cadena de texto y lo muestre poniendo el signo – entre cada carácter sin usar el método replace. Por ejemplo, si tecleo “hola qué tal”, deberá salir “h-o-l-a- -q-u-e- -t-a-l”.
 
 
-"use strict"
-
-let texto = "hola que tal" 
-let textoInvertido = "";
-for(let i = texto.length -1; i>= 0;i--){
-    textoInvertido +=texto[i];
+let cadena = prompt("Introduce una cadena de texto:");
+let numchar = cadena.length;   
+let caracter;
+let i;
+for (i = 0; i < numchar; i++) {
+    caracter = cadena.charAt(i);    
+    if (i == numchar - 1) {
+        document.write(caracter);
+    }
+    else {
+        document.write(caracter + "-");
+    }
 }
-  console.log(textoInvertido);
-
