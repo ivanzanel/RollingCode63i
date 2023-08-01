bfb20a3dde956d223799cf7c65eca5f137f42efe
ivanzanel <62265767+ivanzanel@users.noreply.github.com>
2023-08-01 12:46:03 -0300

    Resolución TP - Ex1

--- ejerciciosTP3/ex1/app.js
+++ ejerciciosTP3/ex1/app.js
@@ -1,11 +1,10 @@
 //1- Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla en forma de lista los doce nombres del arreglo.
 
-
-
-
-
-
 "use strict";
 
+let meses = ['• Enero', '• Febrero', '• Marzo', '• Abril', '• Mayo', '• Junio', '• Julio', '• Agosto', '• Septiembre', '• Octubre ', '• Noviembre', '• Diciembre']
 
-window.alert("Un mensaje");
+
+meses.forEach((mes) => {
+    console.log(mes);
+});
